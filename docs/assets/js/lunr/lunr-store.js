c06edd3a81becbf36b62736b656c5246897573d9
Eli Pousson <eli.pousson@gmail.com>
2018-02-22 12:24:48 -0500

    Replace localhost with correct links

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "A new site for a new project",
         "excerpt":"Welcome to our new site for Baltimore’s Civil Rights Heritage! At Baltimore Heritage, we are passionate about trying out new ways to make our work accessible, engaging and useful. We created this site (using an open-source tool known as [Jekyll](http://jekyllrb.com)) to openly share our writing, research notes, and other resources...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/civil-rights-heritage/new-site/",
-        "teaser":"http://localhost:4000/civil-rights-heritage/assets/images/1870-loc-01767v.jpg"},{
+        "url": "https://baltimoreheritage.github.io/civil-rights-heritage/new-site/",
+        "teaser":"https://baltimoreheritage.github.io/civil-rights-heritage/assets/images/1870-loc-01767v.jpg"},{
         "title": "Documenting the George W. King/Abraham Briscoe House for potential landmark designation",
         "excerpt":"We are working on a study of 1232 Druid Hill Avenue in response to the [recent demolition of the Freedom House](http://baltimoreheritage.org/preservation/freedom-house-demolition-is-a-wake-up-call-for-preservation-in-west-baltimore/) at 1234 Druid Hill Avenue. We've [published the draft study](/civil-rights-heritage/places/1232-druid-hill-avenue/) to help open up the process of our research, writing and documentation of this important landmark in the Upton...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/civil-rights-heritage/joseph-briscoe-house/",
-        "teaser":"http://localhost:4000/civil-rights-heritage/assets/images/1870-loc-01767v.jpg"},{
+        "url": "https://baltimoreheritage.github.io/civil-rights-heritage/joseph-briscoe-house/",
+        "teaser":"https://baltimoreheritage.github.io/civil-rights-heritage/assets/images/1870-loc-01767v.jpg"},{
         "title": "How we organized a South Baltimore Civil Rights walking tour for Digital Harbor High School",
         "excerpt":"Last Friday, I put together a walking tour program for a group of students from Digital Harbor High School who are learning about the history of African Americans and the Civil Rights movement in South Baltimore as part of our ongoing educational outreach around this project. As a resource for...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/civil-rights-heritage/digital-harbor-hs-tour/",
-        "teaser":"http://localhost:4000/civil-rights-heritage/assets/images/1870-loc-01767v.jpg"}]
+        "url": "https://baltimoreheritage.github.io/civil-rights-heritage/digital-harbor-hs-tour/",
+        "teaser":"https://baltimoreheritage.github.io/civil-rights-heritage/assets/images/1870-loc-01767v.jpg"}]
